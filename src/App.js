31222cfbef438ed18237463cfd36af2754e26ecb
土井悠生 <doiyuusei@doiyuuseinoMacBook-Pro.local>
2020-12-15 21:58:11 +0900

    3-9

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = props => {
 
   const [state, setState] = useState(props);
   const { name, price } = state;
+
+  useEffect(() => {
+    console.log('useEffect');
+  })
+
+  useEffect(() => {
+    console.log('useEffect2');
+  }, [])
+
+  useEffect(() => {
+    console.log('useEffect3');
+  }, [name])
 
   return (
     <>
